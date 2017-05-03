7dcf1edc6940ac2004f49e62f23a7aae2340a51e
TiagoPinheiro2580 <a10929@alunos.ipca.pt>
2017-05-03 19:31:16 +0100

    GameScreen States
    Inicialização do projeto
    GameScreen

---
+++ NinjOut/NinjOut/GameScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace NinjOut
+{
+    public class GameScreen
+    {
+        public virtual void Initialize()
+        {
+
+        }
+
+        public virtual void LoadContent (ContentManager Content)
+        {
+
+        }
+
+        public virtual void Update (GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw (SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ NinjOut/NinjOut/ScreenManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NinjOut
+{
+    public class ScreenManager
+    {
+        #region Variaveis 
+
+        GameScreen currentScreen;
+        GameScreen newScreen;
+        //Screen Manager Instance
+
+        private static ScreenManager instance;
+
+        // Saber qual screen está aberto e em qual ordem
+
+        Stack<GameScreen> screenStack = new Stack<GameScreen>();
+
+        //Dimensões do ecran (largura e altura)
+
+        Vector2 dimensions;
+        #endregion
+
+        #region Propriedades 
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ScreenManager();
+                }
+                return instance;
+            }
+        }
+
+        public Vector2 Dimensions
+        {
+            get { return dimensions; }
+            set { dimensions = value; }
+        }
+        #endregion
+
+        #region Main Methods
+
+        public void AddScreen (GameScreen screen)
+        {
+            newScreen = screen;
+            screenStack.Push(screen);
+        }
+
+        public void Initialize ()
+        {
+
+        }
+
+        public void LoadContent (ContentManager Content)
+        {
+
+        }
+
+        public void Update (GameTime gameTime)
+        {
+            currentScreen.Update(gameTime);
+        }
+
+        public void Draw (SpriteBatch spriteBatch)
+        {
+            currentScreen.Draw(spriteBatch);
+        }
+
+        #endregion
+    }
+}
